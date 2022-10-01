0af01698696a130898574c25d6fd52f1653925d5
Ludmilla <ludmillajani@yahoo.com>
2022-10-01 17:18:46 -0400

    First commit

---
+++ employ-system-api/src/test/java/com/refresh/employEmploy/EmploySystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.refresh.employEmploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmploySystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
